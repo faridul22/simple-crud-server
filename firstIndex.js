38599eae0cdeed8a0988507ea8627fc0c8342098
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-08-23 15:45:38 +0600

    full crud operation done

--- firstIndex.js
+++ firstIndex.js
@@ -9,6 +9,11 @@ app.use(cors());
 app.use(express.json());
 
 //---------------MongoDB----------
+// simpleCRUDServer
+// UJWpKAgOJDDqiKxS
+
+
+// const uri = "mongodb+srv://simpleCRUDServer:UJWpKAgOJDDqiKxS@cluster0.xwjksg9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const uri = "mongodb+srv://simpleCRUDServer:UJWpKAgOJDDqiKxS@cluster0.xwjksg9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
@@ -23,35 +28,33 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        const usersCollection = client.db('simpleCrudDB').collection('usersCollection')
 
-        // Get all users
-        app.get('/users', async (req, res) => {
-            const users = await usersCollection.find().toArray();
-            res.send(users)
-        });
+        const usersCollection = client.db("simpleCrudDB").collection("usersCollection");
 
-        // Get a specific user by using id
-        app.get('/users/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await usersCollection.findOne(query);
+        app.get("/users", async (req, res) => {
+            const users = usersCollection.find();
+            const result = await users.toArray();
             res.send(result)
         })
 
-        // Post a User
-        app.post('/users', async (req, res) => {
-            const user = req.body;
-            const result = await usersCollection.insertOne(user)
+        app.get("/users/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await usersCollection.findOne(query)
             res.send(result)
-            console.log(user)
-        });
+        })
 
-        // Update a specific user info
-        app.put('/users/:id', async (req, res) => {
+        app.post("/users", async (req, res) => {
+            const newUser = req.body;
+            console.log(newUser);
+            const result = await usersCollection.insertOne(newUser);
+            res.send(result)
+        })
+
+        app.put("/users/:id", async (req, res) => {
             const id = req.params.id;
             const user = req.body;
-            const filter = { _id: new ObjectId(id) };
+            const filter = { _id: new ObjectId(id) }
             const options = { upsert: true };
             const updatedUser = {
                 $set: {
@@ -60,33 +63,33 @@ async function run() {
                     password: user.password
                 }
             }
-            const result = await usersCollection.updateOne(filter, updatedUser, options);
+            const result = await usersCollection.updateOne(filter, updatedUser, options)
             res.send(result)
+            console.log(id, user)
         })
 
-        // Delete a specific user
-        app.delete('/users/:id', async (req, res) => {
+        app.delete("/users/:id", async (req, res) => {
             const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
+            console.log('Please delete form database', id)
+            const query = { _id: new ObjectId(id) }
             const result = await usersCollection.deleteOne(query);
             res.send(result)
         })
 
-        // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        console.log("You successfully connected to MongoDB!");
     } finally {
-
 
     }
 }
 run().catch(console.dir);
 
-//------------------End----------------
+//---------------End--------------
+
 app.get('/', (req, res) => {
-    res.send('Simple crud server is Running')
+    res.send("Simple CRUD is Running")
 });
 
 app.listen(port, () => {
-    console.log(`Simple crud server is Running on port: ${port}`)
+    console.log(`Simple CRUD is Running on port: ${port}`)
 })
